142b7af37f6807e05ee515a88d771991c4a03e65
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-18 01:56:22 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  result = Hash.new
+   
+  data.each do |attribute, categories|
+    categories.each do |category, pigeons|
+      pigeons.each do |name|
+        if result.include?(name)
+          if result[name].include?(attribute)
+            result[name][attribute] << category.to_s
+          else
+            result[name][attribute] = [] << category.to_s
+          end
+        else
+          result[name] = {}
+          result[name][attribute] = [] << category.to_s
+        end
+      end
+    end
+  end
+  result
 end
